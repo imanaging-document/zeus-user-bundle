518dc16b507e97aa6a575790c8c889c73e20ae4d
n1n1du44 <a.auffray@imanaging-document.fr>
2019-12-26 17:54:43 +0100

    gestion sso

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -5,14 +5,18 @@ namespace Imanaging\ZeusUserBundle\Controller;
 use Imanaging\ZeusUserBundle\Interfaces\ConnexionInterface;
 use Imanaging\ZeusUserBundle\Interfaces\UserInterface;
 use Imanaging\ZeusUserBundle\Login;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ConnexionController extends ImanagingController
 {
   private $login;
+  private $tokenStorage;
 
-  public function __construct(Login $login)
+  public function __construct(Login $login, TokenStorageInterface $tokenStorage)
   {
     $this->login = $login;
+    $this->tokenStorage = $tokenStorage;
   }
 
   public function showTableConnexion(){
@@ -29,5 +33,16 @@ class ConnexionController extends ImanagingController
     }
     return $this->render('@ImanagingZeusUser/access_denied.html.twig', [
     ]);
+  }
+
+  public function getDataForRedirect() {
+    $user = $this->tokenStorage->getToken()->getUser();
+    // on demande a ZEUS le token (sha256 login/tokenAppli+datatime) de cet utilisateur et si utilisateur zeus ou
+    $res = $this->login->getDataForRedirect($user);
+    if (in_array($res['type'], ['zeus', 'core'])) {
+      return new JsonResponse($res);
+    } else {
+      return new JsonResponse(['error_message' => 'Un utilisateur local ne peux pas être redirigé'], 500);
+    }
   }
 }
--- src/Login.php
+++ src/Login.php
@@ -11,6 +11,7 @@ namespace Imanaging\ZeusUserBundle;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
+use http\Client\Curl\User;
 use Imanaging\ApiCommunicationBundle\ApiCoreCommunication;
 use Imanaging\ApiCommunicationBundle\ApiZeusCommunication;
 use Imanaging\ZeusUserBundle\Interfaces\ConnexionInterface;
@@ -90,6 +91,58 @@ class Login
     return false;
   }
 
+
+  public function canLogSso($login, $token) {
+    // on vérifie dans la base local si le user existe, sinon on check sur ZEUS
+    $user = $this->em->getRepository(UserInterface::class)->findOneBy(['login' => $login]);
+
+    if ($user instanceof UserInterface){
+      if ($user->isUtilisateurZeus()) {
+        // on check par API
+        $url = '/connexion-sso?login='.$login.'&token='.$token;
+        $response = $this->apiZeusCommunication->sendGetRequest($url);
+
+        if ($response->getHttpCode() == 200) {
+          $userLogin = json_decode($response->getData());
+
+          if ($userLogin != "") {
+            $userFound = $this->em->getRepository(UserInterface::class)->findOneBy(array('login' => $userLogin));
+            if ($userFound instanceof UserInterface) {
+              return $userFound;
+            }
+          }
+        }
+      } elseif ($user->isUtilisateurCore()){
+        // on check par API sur le CORE
+        $now = new DateTime();
+        $nowFormat = $now->format('YmdHis');
+        $tokenFormatted = hash('sha256', $this->apiCoreCommunication->getApiCoreToken());
+
+        $url = '/can-log-sso?login='.$login.'&tokenUser='.$token.'&token='.$tokenFormatted.'&token_date'.$nowFormat;
+        $response = $this->apiCoreCommunication->sendGetRequest($url);
+        if ($response->getHttpCode() == 200) {
+          $userLogin = json_decode($response->getData());
+
+          if ($userLogin != "") {
+            $userFound = $this->em->getRepository(UserInterface::class)->findOneBy(array('login' => $userLogin));
+            if ($userFound instanceof UserInterface) {
+              return $userFound;
+            }
+          }
+        }
+      } else {
+        if ($this->encoder->isPasswordValid($user, $password)){
+          return $user;
+        } else {
+          $this->createConnexion('mdp_incorrect', $user, $user->getLogin(), $ipAddress);
+        }
+      }
+    } else {
+      $this->createConnexion($user, $login, 'compte_inexistant', $ipAddress);
+    }
+    return false;
+  }
+
   /**
    * @param $user
    * @param $login
@@ -114,6 +167,62 @@ class Login
         $this->em->persist($connexion);
         $this->em->flush();
       }
+    }
+  }
+
+  public function getDataForRedirect(UserInterface $user)
+  {
+    if ($user->isUtilisateurCore()) {
+      return $this->getDataForRedirectCore($user);
+    } elseif ($user->isUtilisateurZeus()) {
+      return $this->getDataForRedirectZeus($user);
+    } else {
+      return ['type' => 'local'];
+    }
+  }
+
+  private function getDataForRedirectCore(UserInterface $user)
+  {
+    return [
+      'type' => 'core',
+      'login' => $user->getLogin(),
+      'token' => $this->getTokenFromCore($user)
+    ];
+  }
+
+  private function getDataForRedirectZeus(UserInterface $user)
+  {
+    return [
+      'type' => 'zeus',
+      'login' => $user->getLogin(),
+      'token' => $this->getTokenFromZeus($user)
+    ];
+  }
+
+  private function getTokenFromZeus(UserInterface $user)
+  {
+    // on check par API
+    $url = '/connexion/token/' . $user->getLogin() . '?login='.$this->apiZeusCommunication->getApiZeusLogin().'&password='.$this->apiZeusCommunication->getApiZeusPassword();
+    $response = $this->apiZeusCommunication->sendGetRequest($url);
+    if ($response->getHttpCode() == 200) {
+      return json_decode($response->getData());
+    } else {
+      return '';
+    }
+  }
+
+  private function getTokenFromCore(UserInterface $user)
+  {
+    // on check par API
+    $now = new DateTime();
+    $nowFormat = $now->format('YmdHis');
+    $tokenFormatted = hash('sha256', $this->apiCoreCommunication->getApiCoreToken());
+    $url = '/connexion/token/' . $user->getLogin() . '?token='.$tokenFormatted.'&token_date'.$nowFormat;
+    $response = $this->apiCoreCommunication->sendGetRequest($url);
+    if ($response->getHttpCode() == 200) {
+      return json_decode($response->getData());
+    } else {
+      return 'error';
     }
   }
 }
