03b62d89c3257f771338134ba084f0be11db48ee
Rémi Zanzi <r.zanzi@imanaging-document.fr>
2020-12-16 12:16:53 +0100

    update DestinataireMailIntefface

--- src/Interfaces/DestinataireMailInterface.php
+++ src/Interfaces/DestinataireMailInterface.php
@@ -9,6 +9,22 @@ interface DestinataireMailInterface
 
   public function setId($id);
 
+  public function getNom();
+
+  public function setNom(string $nom);
+
+  public function getPrenom();
+
+  public function setPrenom(string $prenom);
+
+  public function getMail();
+
+  public function setMail(string $mail);
+
+  public function getNumero();
+
+  public function setNumero(string $numero);
+
   public function setUser(UserInterface $user);
 
   public function getUser();
